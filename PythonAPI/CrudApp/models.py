9155c048b77800578e0682a8a27546a3365c96a8
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2021-10-29 01:50:34 -0300

    adiciona models

--- PythonAPI/CrudApp/models.py
+++ PythonAPI/CrudApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+
+class Editora(models.Model):
+    EditoraId = models.AutoField(primary_key=True)
+    NomeEditora = models.CharField(max_length=100)
+    LocalEditora = models.CharField(max_length=100)
+
+
+class EditoraLivro(models.Model):
+    Id_Editora = models.CharField(max_length=100)
+    Id_Livro = models.CharField(max_length=100)
+
+
+class Livro(models.Model):
+    LivroId = models.AutoField(primary_key=True)
--- PythonAPI/PythonAPI/settings.py
+++ PythonAPI/PythonAPI/settings.py
@@ -37,9 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'CrudApp.apps.CrudappConfig'
 ]
 
+CORS_ORIGIN_ALLOW_ALL=True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
