28653a7f35cc985f619bafc9405d1f1a7f51372e
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2021-11-17 14:59:33 -0300

    adiciona o swagger na API

--- djangoRest/pbl/urls.py
+++ djangoRest/pbl/urls.py
@@ -19,6 +19,25 @@ from django.urls import path, include
 from rest_framework import routers
 from editoraApp.api import viewsets as editoraviewsets
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Problem Based Learning VII Uninter Engenharia da Computação",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 route = routers.DefaultRouter()
 route.register(r'pbl/editora', editoraviewsets.EditoraViewSet,
                basename="editora")
@@ -26,5 +45,7 @@ route.register(r'pbl/editora', editoraviewsets.EditoraViewSet,
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(route.urls))
+    path('', include(route.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
