fcf8843b236fa9cb70f3bf1b9e9281f94b3c87ff
Vinicius Marcante <vinirosamarcante@gmail.com>
2021-11-22 22:18:39 -0300

    limpando branch para atualização

--- PythonAPI/CrudApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, urls
-from django.urls.conf import path
-from CrudApp import views
-
-urlpatterns = [
-    path('editora', views.EditoraApi),
-    path('editora/<int:id>', views.EditoraApi),
-]
