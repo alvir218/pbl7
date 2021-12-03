104df2bdd1f64b48c1ab484c551d179a46cfe0b7
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2021-12-02 23:51:06 -0300

    ajuste para FK de editora em livros

--- djangoRest/Livros/models.py
+++ djangoRest/Livros/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 from disciplinaApp.models import Disciplina
-
-
+from editoraApp.models import Editora
 
 
 # Create your models here.
@@ -10,6 +9,8 @@ from disciplinaApp.models import Disciplina
 
 class Livros(models.Model):
     cod_livro = models.AutoField(primary_key=True)
+    editora_id = models.ForeignKey(
+        Editora, related_name='Editora', on_delete=models.CASCADE)
     ISBN = models.CharField(max_length=100)
     Titulo_livro = models.CharField(max_length=100)
     autor_livro = models.CharField(max_length=100)
@@ -26,9 +27,10 @@ class Biblioteca(models.Model):
     sigla_biblioteca = models.CharField(max_length=100)
     modalidade_virtual = models.CharField(max_length=100)
 
-class DisciplinaLivro(models.Model):
-    DisciplinaId = models.ForeignKey(Disciplina,related_name='DisciplinaLivro',on_delete=models.CASCADE)
-    cod_livro = models.ForeignKey(Livros,related_name='DisciplinaLivro',on_delete=models.CASCADE)
-    bibliografia_basica = models.CharField(max_length=100)
-    
 
+class DisciplinaLivro(models.Model):
+    DisciplinaId = models.ForeignKey(
+        Disciplina, related_name='DisciplinaLivro', on_delete=models.CASCADE)
+    cod_livro = models.ForeignKey(
+        Livros, related_name='DisciplinaLivro', on_delete=models.CASCADE)
+    bibliografia_basica = models.CharField(max_length=100)
